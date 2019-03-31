dc3db3f8b4a4b52e932735dd225cfbfb624296a6
LycanCoder <email.yong@qq.com>
2019-03-31 17:26:17 +0800

    V1.0.0

--- app/Http/Controllers/Api/IndexController.php
+++ app/Http/Controllers/Api/IndexController.php
@@ -13,9 +13,48 @@ use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
+    /**
+     * index 接口入口
+     * @author Lycan <LycanCoder@gmail.com>
+     * @date 2019/3/31
+     *
+     * @param Request $request
+     */
     public function index(Request $request)
     {
         $getData = $request->all();
-        return $getData;
+        $method = $getData["method"];
+        if (!method_exists($this, $method)) {
+            echo json_encode(array("code" => 1, "msg" => $method."方法不存在", "data" => $getData));
+            exit();
+        }
+
+        unset($getData["timestamp"], $getData["nonce"], $getData["signature"], $getData["src"], $getData["method"]);
+        $this->$method($getData);
+    }
+
+    /**
+     * returnJson 接口请求返回数据
+     * @author Lycan <LycanCoder@gmail.com>
+     * @date 2019/3/31
+     *
+     * @param array $data
+     */
+    private function returnJson($data = array())
+    {
+        echo json_encode($data);
+        exit();
+    }
+
+    /**
+     * home 请求方法
+     * @author Lycan <LycanCoder@gmail.com>
+     * @date 2019/3/31
+     *
+     * @param array $getData
+     */
+    private function home($getData = array())
+    {
+        $this->returnJson(return_code(0,"success", $getData));
     }
 }
--- app/Http/Middleware/ApiParameter.php
+++ app/Http/Middleware/ApiParameter.php
@@ -15,35 +15,41 @@ class ApiParameter
      */
     public function handle($request, Closure $next)
     {
-        // http://localhost/api/index?timestamp=1547047192215&nonce=6020526690292680&signature=6b2fd274073f1de2308403cb6e0cb78f89f3b66c9dbf647cb7cf224a270b7de4&src=lyite
+        // http://localhost/api/index?timestamp=1547047192215&nonce=6020526690292680&signature=6b2fd274073f1de2308403cb6e0cb78f89f3b66c9dbf647cb7cf224a270b7de4&src=lyite&method=index
         $token = 'lyite1001'; // 可以由英文和数字组成，自行指定
         $src = 'lyite'; // 自行指定
         $getData = $request->all();
+        if (!isset($getData['timestamp']) || !isset($getData['nonce']) || !isset($getData['signature']) || !isset($getData['src']) || !isset($getData['method'])) {
+            echo json_encode(array("code" => 1, "msg" => "url请求参数错误", "data" => $getData));
+            exit();
+        }
+
         $timestamp = $getData['timestamp']; // 13位长度时间戳
         $nonce = $getData['nonce']; // 随机数
+
+        if (13 != strlen($timestamp)) {
+            echo json_encode(array("code" => 1, "msg" => "时间格式错误", "data" => $getData));
+            exit();
+        }
+
+        if (16 != strlen($nonce)) {
+            echo json_encode(array("code" => 1, "msg" => "随机数格式错误", "data" => $getData));
+            exit();
+        }
+
         $signature = self::SHA256("{$timestamp}{$nonce}{$token}");
-        if ( !isset($getData['timestamp']) || !isset($getData['nonce']) || !isset($getData['signature']) || !isset($getData['src']) ) {
-            echo 'url请求参数错误';
-            exit();
-        }
-
-        if (13 != strlen($getData['timestamp'])) {
-            echo '时间格式错误';
-            exit();
-        }
-
-        if (16 != strlen($getData['nonce'])) {
-            echo '随机数格式错误';
-            exit();
-        }
-
         if ($signature != $getData['signature']) {
-            echo '签名格式错误';
+            echo json_encode(array("code" => 1, "msg" => "签名格式错误", "data" => $getData));
             exit();
         }
 
         if ($src != $getData['src']) {
-            echo 'src格式错误';
+            echo json_encode(array("code" => 1, "msg" => "src格式错误", "data" => $getData));
+            exit();
+        }
+
+        if ("" == $getData['method']) {
+            echo json_encode(array("code" => 1, "msg" => "方法不能为空", "data" => $getData));
             exit();
         }
 
